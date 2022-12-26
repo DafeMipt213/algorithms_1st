2bc0ac7f5046bea1a82ee12a5d61fae6ca48f1b2
Ivan Khokhlov <lostpointmeister@gmail.com>
2022-12-26 16:12:13 +0000

    add stack task

--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -3,4 +3,9 @@
 
 #include "sort.hpp"
 
-TEST(SortTest, Simple) { ASSERT_EQ(Sort({}), std::vector<int>{}); }
+TEST(SortTest, Simple) {
+  ASSERT_EQ(Sort({}), std::vector<int>{});
+  ASSERT_EQ(Sort({1}), std::vector<int>{1});
+  ASSERT_EQ(Sort({1, 2, 3}), std::vector<int>{1, 2, 3});
+  ASSERT_EQ(Sort({3, 2, 1}), std::vector<int>{1, 2, 3});
+}

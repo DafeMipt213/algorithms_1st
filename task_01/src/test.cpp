a68ace007ee77e7409fee63165edbe5209285907
Ivan Khokhlov <lostpointmeister@gmail.com>
2023-02-11 08:43:22 +0000

    homework

--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -8,7 +8,7 @@
 TEST(StackTest, Simple) {
   Stack stack;
   stack.Push(1);              // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
+  ASSERT_EQ(stack.Pop(), 2);  // Stack []
   stack.Push(1);              // Stack [1]
   stack.Push(2);              // Stack [1, 2]
   ASSERT_EQ(stack.Pop(), 2);  // Stack [1]

abe618b06677a1fc00867d10a1a0ef5a69d11479
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-03-12 13:27:25 +0000

    fix again

--- additional_tasks/temperature/src/utils.hpp
+++ additional_tasks/temperature/src/utils.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
 #include <vector>
-
+#include <stack>
 std::vector<int> CalculateDaysBeforWarmup(const std::vector<float>& temerature);
--- task_03/src/main.cpp
+++ task_03/src/main.cpp
@@ -4,7 +4,7 @@
 #include "sort.hpp"
 int main() {
   std::cout << std::endl << std::endl << std::endl << std::endl;
-  std::vector<int> a = {1, 1, 4, 3, 6, 7, 5, 8, 9, 1, 2};
+  std::vector<int> a = {1, 1, 4, 3, 6, 7, 5, 8, 9, 1, 2};  // NOLINT
   for (int i = 0; i < a.size(); ++i) {
     std::cout << a[i] << " ";
   }

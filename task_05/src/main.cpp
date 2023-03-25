beca9f144a8fe5df9408cd3a449256bc1d99fff9
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-03-25 13:02:30 +0000

    fixed

--- task_05/src/main.cpp
+++ task_05/src/main.cpp
@@ -1,10 +1,4 @@
 #include <iostream>
+
 #include "tree.hpp"
-int main() {
-    Tree ntr;
-    ntr.InsertOrUpdate(7, 11);
-    ntr.InsertOrUpdate(6, 7);
-    ntr.InsertOrUpdate(6, 5);
-    std::cout << ntr.Find(7) << std::endl;
-    std::cout << ntr.Find(6) << std::endl;
-}
+int main() {}

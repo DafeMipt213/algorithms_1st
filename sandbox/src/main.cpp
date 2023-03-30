54bda0c797af7e9462d1d7098689847b8a433f4c
SvetoCopy <65361271+SvetoCopy@users.noreply.github.com>
2023-03-30 18:10:22 +0000

    task 1-3

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 #include <vector>
 
-class Tree{
-
-private:
-    struct Node{
-        Node* parent = ;
-        Node* left_child = ;
-        Node* right_child = ;
-        int Key;
-    }
-}
-
 int main() { 
     
     return 0; 
-    }
+}

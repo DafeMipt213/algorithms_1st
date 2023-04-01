9a1461d6f5be9084a9a16ba472628b072f89b600
G00sya <118297271+G00sya@users.noreply.github.com>
2023-04-01 07:24:06 +0000

    pravka

--- task_03/src/sort.cpp
+++ task_03/src/sort.cpp
@@ -4,7 +4,7 @@ void quickSort(std::vector<int>::iterator array, int low, int hight) {
   int i = low;
   int j = hight;
   int pivot = array[(i + j) / 2];
-  int temp;
+  int temp{0};
 
   while (i <= j) {
     while (array[i] < pivot) i++;
@@ -29,7 +29,7 @@ void printArr(std::vector<int>::iterator array, int n) {
 std::vector<int> Sort(std::vector<int> vec) {
   if (vec.size() == 0) return {};
   std::vector<int>::iterator i = vec.begin();
-  quickSort(i, 0, vec.size() - 1);
-  printArr(vec.begin(), vec.size());
+  quickSort(i, 0, (int)vec.size() - 1);
+  printArr(vec.begin(), (int)vec.size());
   return vec;
 }

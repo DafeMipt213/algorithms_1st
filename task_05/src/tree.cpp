0df9dfe6deb3c35aaffe02726b8778174ae35ac6
ShatunovEvgeniy <118297183+ShatunovEvgeniy@users.noreply.github.com>
2023-04-01 07:20:40 +0000

    tree was commented

--- task_05/src/tree.cpp
+++ task_05/src/tree.cpp
@@ -1,25 +1,25 @@
-#include "tree.hpp"
+// #include "tree.hpp"
 
-#include <exception>
-#include <stdexcept>
+// #include <exception>
+// #include <stdexcept>
 
-Tree::Tree() {}
+// Tree::Tree() {}
 
-bool Tree::Insert(int key, int value) {
-  if (this->Find(key)) return false;
+// bool Tree::Insert(int key, int value) {
+//   if (this->Find(key)) return false;
 
-  return true;
-}
+//   return true;
+// }
 
-void Tree::InsertOrUpdate(int key, int value) {}
+// void Tree::InsertOrUpdate(int key, int value) {}
 
-int Tree::Find(int key) const {
-  if (root == nullptr) throw std::out_of_range("Tree is empty");
-  if (root->key == key) return root->data;
-  if (root->left_child == nullptr and root->right_child == nullptr)
-    throw std::runtime_error("Element hasn't been found");
-  else if (key < root->key)
-    return this->Find(key, root->left_child);
-  else
-    return this->Find(key, root->right_child);
-}
+// int Tree::Find(int key) const {
+//   if (root == nullptr) throw std::out_of_range("Tree is empty");
+//   if (root->key == key) return root->data;
+//   if (root->left_child == nullptr and root->right_child == nullptr)
+//     throw std::runtime_error("Element hasn't been found");
+//   else if (key < root->key)
+//     return this->Find(key, root->left_child);
+//   else
+//     return this->Find(key, root->right_child);
+// }
--- task_05/src/tree.hpp
+++ task_05/src/tree.hpp
@@ -1,19 +1,19 @@
-#pragma once
+// #pragma once
 
-class Tree {
- private:
-  struct Node{
-    Node* parent;
-    Node* left_child{nullptr};
-    Node* right_child{nullptr};
-    int key;
-    int data;
-  };
-  int Find(int key, Node* root) const;
-  Node* root{nullptr};
- public:
-  Tree();
-  bool Insert(int key, int value);
-  void InsertOrUpdate(int key, int value);
-  int Find(int key) const;
-};
+// class Tree {
+//  private:
+//   struct Node{
+//     Node* parent;
+//     Node* left_child{nullptr};
+//     Node* right_child{nullptr};
+//     int key;
+//     int data;
+//   };
+//   int Find(int key, Node* root) const;
+//   Node* root{nullptr};
+//  public:
+//   Tree();
+//   bool Insert(int key, int value);
+//   void InsertOrUpdate(int key, int value);
+//   int Find(int key) const;
+// };

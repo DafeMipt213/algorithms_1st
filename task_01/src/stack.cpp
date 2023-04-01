a705c228721eb0accc46764871ad906bc1eff046
Dememtiy <72860749+Dememtiy@users.noreply.github.com>
2023-04-01 10:39:54 +0000

    Fixed format issues

--- task_01/src/stack.cpp
+++ task_01/src/stack.cpp
@@ -2,33 +2,27 @@
 
 #include <algorithm>
 
-void Stack::Push(int value) { 
-  _data.push_back(value); 
-}
+void Stack::Push(int value) { _data.push_back(value); }
 
-int Stack::Pop() { 
-  int ret_value = _data[_data.size()-1];
+int Stack::Pop() {
+  int ret_value = _data[_data.size() - 1];
   _data.erase(_data.begin() + _data.size() - 1);
   return ret_value;
 }
 
 void MinStack::Push(int value) {
-  if (_data.size() > 0)
-  {
+  if (_data.size() > 0) {
     int min_num = std::min(value, _data[0].second);
     _data.push_back(std::pair<int, int>(value, min_num));
-  }
-  else{
+  } else {
     _data.push_back(std::pair<int, int>(value, value));
   }
 }
 
 int MinStack::Pop() {
-  int ret_value = _data[_data.size()-1].first;
+  int ret_value = _data[_data.size() - 1].first;
   _data.erase(_data.begin() + _data.size() - 1);
   return ret_value;
 }
 
-int MinStack::GetMin() {
-    return _data[_data.size()-1].second;
-}
+int MinStack::GetMin() { return _data[_data.size() - 1].second; }
--- task_03/src/sort.cpp
+++ task_03/src/sort.cpp
@@ -1,48 +1,42 @@
 #include "sort.hpp"
 
-#include <vector>
 #include <stack>
+#include <vector>
 
+int partition(std::vector<int>& data, int l, int r) {
+  // Выбираем рандомный pivot
+  int pivot_index = l + rand() % (r - l + 1);
+  int pivot = data[pivot_index];
+  std::swap(data[pivot_index], data[r]);
 
-int partition(std::vector<int>& data, int l, int r)
-{
-    // Выбираем рандомный pivot
-    int pivot_index = l + rand() % (r - l + 1);
-    int pivot = data[pivot_index];
-    std::swap(data[pivot_index], data[r]);
-
-    int i = l - 1;
-    for (int j = l; j < r; j++)
-    {
-        if (data[j] <= pivot)
-        {
-            i++;
-            std::swap(data[i], data[j]);
-        }
+  int i = l - 1;
+  for (int j = l; j < r; j++) {
+    if (data[j] <= pivot) {
+      i++;
+      std::swap(data[i], data[j]);
     }
-    std::swap(data[i+1], data[r]);
-    return i+1;
+  }
+  std::swap(data[i + 1], data[r]);
+  return i + 1;
 }
 
 // Quick Sort без рекурсии
 std::vector<int> Sort(const std::vector<int>& data) {
-    std::vector<int> sorted_data = data;
-    std::stack<std::pair<int, int>> s;
-    s.push({0, data.size()-1});
+  std::vector<int> sorted_data = data;
+  std::stack<std::pair<int, int>> s;
+  s.push({0, data.size() - 1});
 
-    while (!s.empty())
-    {
-        int l = s.top().first;
-        int r = s.top().second;
-        s.pop();
+  while (!s.empty()) {
+    int l = s.top().first;
+    int r = s.top().second;
+    s.pop();
 
-        if (l >= r)
-            continue;
+    if (l >= r) continue;
 
-        int pivot_index = partition(sorted_data, l, r);
+    int pivot_index = partition(sorted_data, l, r);
 
-        s.push({pivot_index + 1, r});
-        s.push({l, pivot_index - 1});
-    }
-    return sorted_data;
+    s.push({pivot_index + 1, r});
+    s.push({l, pivot_index - 1});
+  }
+  return sorted_data;
 }

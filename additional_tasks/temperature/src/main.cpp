343847ce06186745158e98d0da9edcf7b61351fe
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-04-21 11:26:05 +0000

    Nolint temps

--- additional_tasks/temperature/src/main.cpp
+++ additional_tasks/temperature/src/main.cpp
@@ -6,7 +6,7 @@
 
 int main() {
   std::vector<float> days{
-      4, 5, 3,  12, 3,
+      4, 5, 3,  12, 3,    //-> {1, 3, 3, 9, 5, 7, 9, 9, 0} NOLINT
       6, 4, 11, 11, 13};  //-> {1, 3, 3, 9, 5, 7, 9, 9, 0} NOLINT
   std::vector<int> warm_ups = CalculateDaysBeforWarmup(days);
   for (int i = 0; i < days.size(); ++i) {

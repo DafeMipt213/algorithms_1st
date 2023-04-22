9f883e59577d19b9a3e3a3f62c19f3c071c2f762
Ivan Khokhlov <lostpointmeister@gmail.com>
2023-04-22 11:16:25 +0000

    remove debug

--- additional_tasks/find_sum_numbers/src/main.cpp
+++ additional_tasks/find_sum_numbers/src/main.cpp
@@ -1,10 +1,4 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-  std::vector<int> v = {1, 2, 3, 4, 5, 6, 8, 10, 11};
-  std::cout << v[0];
-  std::vector<int> v1 = {1, 2, 3, 5, 78, 1};
-  std::cout << v1[0];
-  return 0;
-}
+int main() { return 0; }

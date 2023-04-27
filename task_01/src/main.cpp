c6d915a9810022363e9c96657445bf63ef51c075
MrSnoki <119107317+MrSnoki@users.noreply.github.com>
2023-04-27 14:48:53 +0000

    Task 1-3

--- task_01/src/main.cpp
+++ task_01/src/main.cpp
@@ -1,3 +1,36 @@
 #include <iostream>
+#include <vector>
 
-int main() { return 0; }
+using namespace std;
+
+class StackWithMin {
+public:
+    void push(int val) {
+        stack.push_back(val);
+        if (min_stack.empty() || val <= min_stack.back()) {
+            min_stack.push_back(val);
+        }
+    }
+
+    void pop() {
+        if (stack.empty()) {
+            return;
+        }
+        if (stack.back() == min_stack.back()) {
+            min_stack.pop_back();
+        }
+        stack.pop_back();
+    }
+
+    int top() {
+        return stack.back();
+    }
+
+    int getMin() {
+        return min_stack.back();
+    }
+
+private:
+    vector<int> stack;
+    vector<int> min_stack;
+};

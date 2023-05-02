725d01c5e17cf7258f8394e401aa13feafb9be87
Dememtiy <72860749+Dememtiy@users.noreply.github.com>
2023-05-02 10:59:24 +0000

    finally corrected bug with 5 task

--- task_05/src/tree.hpp
+++ task_05/src/tree.hpp
@@ -1,18 +1,13 @@
 #pragma once
 
 class Tree {
-    private:
-    struct Node {
-        /*Node* parent = nullptr;
-        Node* left_child = nullptr;
-        Node* right_child = nullptr;*/
-        int key;
-        char data;
-    };
-    /*public:
-    Node* root = nullptr;*/
-    Tree();
-    bool Insert(int key, int value);
-    void InsertOrUpdate(int key, int value);
-    int Find(int key) const;
+ public:
+  /*
+    Node* root = nullptr;
+  */
+  Tree();
+
+  bool Insert(int key, int value);
+  void InsertOrUpdate(int key, int value);
+  int Find(int key) const;
 };

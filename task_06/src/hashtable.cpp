e0a625ec0116b8bf4f498ae204440852d432ace4
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-05-06 18:46:56 +0000

    HASHTABLE

--- task_06/src/hashtable.cpp
+++ task_06/src/hashtable.cpp
@@ -1,25 +1,84 @@
 #include "hashtable.hpp"
 
 #include <cstddef>
+#include <functional>
+#include <stdexcept>
 #include <string>
+#include <unordered_set>
 
-HashTable::HashTable() {}
+HashTable::HashTable() {
+  data_ = std::vector<std::vector<std::pair<std::string, int>>>(10);
+}
 
+void HashTable::Expand() {
+  std::vector<std::vector<std::pair<std::string, int>>> tmp_data =
+      std::vector<std::vector<std::pair<std::string, int>>>{};
+  tmp_data.resize(data_.size() * 2);
+  for (int i = 0; i < data_.size(); ++i) {
+    for (int j = 0; j < data_[i].size(); ++j)
+      tmp_data[(std::hash<int>{}(data_[i][j].second)) % tmp_data.size()]
+          .push_back(data_[i][j]);
+  }
+  data_ = tmp_data;
+}
+bool HashTable::Contains(int key) {
+  int i = 0;
+  std::vector<std::pair<std::string, int>> curr =
+      data_[(std::hash<int>{}(key)) % data_.size()];
+  while (i < curr.size()) {
+    i += 1;
+    if (curr[i].second == key) {
+      return true;
+    }
+  }
+  return false;
+}
 bool HashTable::Insert(const std::string& key, int value) {
-  if (data_.contains(key)) return false;
-  data_[key] = value;
+  if (this->Contains(value)) return false;
+  if (data_[(std::hash<int>{}(value)) % data_.size()].size() > 5) {
+    this->Expand();
+  }
+  data_[(std::hash<int>{}(value)) % data_.size()].push_back(
+      std::pair<std::string, int>(key, value));
   return true;
 }
 
 void HashTable::InsertOrUpdate(const std::string& key, int value) {
-  data_[key] = value;
+  bool met = false;
+  for (int i = 0; i < data_[(std::hash<int>{}(value)) % data_.size()].size();
+       ++i) {
+    if (data_[(std::hash<int>{}(value)) % data_.size()][i].second == value) {
+      data_[(std::hash<int>{}(value)) % data_.size()][i].first = key;
+      return;
+    }
+  }
+  data_[(std::hash<int>{}(value)) % data_.size()].push_back(
+      std::pair<std::string, int>(key, value));
 }
 
-void HashTable::Remove(const std::string& key) {
-  auto it = data_.find(key);
-  data_.erase(it);
+void HashTable::Remove(const int value) {
+  bool met = false;
+  for (int i = 0; i < data_[(std::hash<int>{}(value)) % data_.size()].size();
+       ++i) {
+    if (data_[(std::hash<int>{}(value)) % data_.size()][i].second == value) {
+      data_[(std::hash<int>{}(value)) % data_.size()].erase(
+          data_[(std::hash<int>{}(value)) % data_.size()].begin() + i);
+      return;
+    }
+  }
+  throw std::out_of_range("No such element in hash table");
 }
-
-int HashTable::Find(const std::string& key) const { return data_.at(key); }
+std::string HashTable::Find(const int key) const {
+  int i = 0;
+  std::vector<std::pair<std::string, int>> curr =
+      data_[(std::hash<int>{}(key)) % data_.size()];
+  while (i < curr.size()) {
+    i += 1;
+    if (curr[i].second == key) {
+      return curr[i].first;
+    }
+  }
+  throw std::out_of_range("No such element in hash table");
+}
 
 size_t HashTable::Size() const { return data_.size(); }
--- task_06/src/hashtable.hpp
+++ task_06/src/hashtable.hpp
@@ -2,17 +2,19 @@
 
 #include <cstddef>
 #include <string>
-#include <unordered_map>
+#include <vector>
 
 class HashTable {
  public:
   HashTable();
   bool Insert(const std::string& key, int value);
   void InsertOrUpdate(const std::string& key, int value);
-  void Remove(const std::string& key);
-  int Find(const std::string& key) const;
+  void Remove(const int value);
+  std::string Find(const int key) const;
   size_t Size() const;
+  void Expand();
+  bool Contains(int key);
 
  private:
-  std::unordered_map<std::string, int> data_;
+  std::vector<std::vector<std::pair<std::string, int>>> data_;
 };

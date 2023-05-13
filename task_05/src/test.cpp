6c2c2299d31f6a62ed5cb0dea89e497c55e20bd3
SvetoCopy <65361271+SvetoCopy@users.noreply.github.com>
2023-05-13 09:57:32 +0000

    tests

--- task_05/src/test.cpp
+++ task_05/src/test.cpp
@@ -5,4 +5,14 @@
 
 #include "tree.hpp"
 
-TEST(Tree, Simple) { Tree tree; }
+TEST(Tree, Simple) {
+  Tree tree;
+  tree.insert(5, 10);
+  tree.insert(6, 12);
+  tree.insert(7, 14);
+  tree.insert(11, 22);
+  ASSERT_EQ(tree.root->Key, 6);
+  ASSERT_EQ(tree.root->right_child->Key, 7);
+  ASSERT_EQ(tree.root->right_child->right_child->Key, 11);
+  ASSERT_EQ(tree.root->left_child->Key, 5);
+}
--- task_06/src/test.cpp
+++ task_06/src/test.cpp
@@ -13,4 +13,18 @@ TEST(HashTable, Simple) {
   data.InsertOrUpdate("aaaa", 2);
   ASSERT_EQ(data.Find("aaaa"), 2);
   ASSERT_EQ(data.Size(), 1);
+  data.Remove("aaaa");
+  ASSERT_EQ(data.Size(), 0);
+}
+TEST(HashTable, Even) {
+  HashTable data;
+  data.Insert("aaaa", 1);
+  data.Insert("vvvv", 2);
+  data.InsertOrUpdate("aaaa", 3);
+  data.Insert("bbbb", 4);
+  ASSERT_EQ(data.Size(), 3);
+  ASSERT_EQ(data.Find("aaaa"), 3);
+  data.Remove("bbbb");
+  ASSERT_EQ(data.Size(), 2);
+  ASSERT_EQ(data.Find("vvvv"), 2);
 }

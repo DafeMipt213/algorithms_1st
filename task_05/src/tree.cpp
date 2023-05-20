38a5543c704e43334771d8efaf7d5dc3000de542
Climentorii <63839123+Climentorii@users.noreply.github.com>
2023-05-20 08:03:17 +0000

    done task4

--- task_05/src/tree.cpp
+++ task_05/src/tree.cpp
@@ -1,9 +1,26 @@
 #include "tree.hpp"
 
-Tree::Tree() {}
+Node* Node::merge(Node *l, Node *r) {
+  if (!l) return r;
+  if (!r) return l;
+  if (l->prior > r->prior) {
+    l->r_child = merge(l->r_child, r);
+    return l;
+  } else {
+    r->l_child = merge(l, r->l_child);
+    return r;
+  }
+}
 
-bool Tree::Insert(int key, int value) { return true; }
-
-void Tree::InsertOrUpdate(int key, int value) {}
-
-int Tree::Find(int key) const { return 0; }
+std::pair<Node*, Node*> split(Node* p, int x) {
+  if (!p) return {0, 0};
+  if (p->value <= x) {
+    std::pair<Node*, Node*> q = split(p->r_child, x);
+    p->r_child = q.first;
+    return {p, q.second};
+  } else {
+    std::pair<Node*, Node*> q = split(p->l_child, x);
+    p->l_child = q.second;
+    return {q.first, p};
+  }
+}
--- task_05/src/tree.hpp
+++ task_05/src/tree.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
-class Tree {
- public:
-  Tree();
-  bool Insert(int key, int value);
-  void InsertOrUpdate(int key, int value);
-  int Find(int key) const;
+#include <cstdlib>
+#include <map>
+struct Node {
+  int value, prior;
+  Node* l_child = 0;
+  Node* r_child = 0;
+  Node (int _key) { value = _key, prior = rand(); }
+
+  Node* merge(Node* l, Node* r);
+
+  std::pair<Node*, Node*> split(Node* p, int x);
 };
+

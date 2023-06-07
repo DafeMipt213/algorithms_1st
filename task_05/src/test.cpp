616c003a125cfc59ba868b9e1ad729824e5063a1
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-06-07 13:22:42 +0000

    tree_test

--- task_05/src/test.cpp
+++ task_05/src/test.cpp
@@ -5,4 +5,13 @@
 
 #include "tree.hpp"
 
-TEST(Tree, Simple) { Tree tree; }
+TEST(Tree, Simple) {
+  Tree tree;
+  ASSERT_EQ(tree.Insert(4, 5), true);
+  ASSERT_EQ(tree.Insert(6, 6), true);
+  ASSERT_EQ(tree.Insert(4, 6), false);
+  ASSERT_EQ(tree.Find(4), 5);
+  tree.Remove(4);
+  tree.InsertOrUpdate(6, 78);
+  ASSERT_EQ(tree.Find(6), 78);
+}

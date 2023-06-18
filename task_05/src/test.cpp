116d014bf18ba883188a3d49eb1874577ab5442f
Climentorii <63839123+Climentorii@users.noreply.github.com>
2023-06-18 20:05:29 +0000

    task 5

--- task_05/src/test.cpp
+++ task_05/src/test.cpp
@@ -5,4 +5,16 @@
 
 #include "tree.hpp"
 
-// TEST(Tree, Simple) { Tree tree; }
+TEST(Tree, Simple) {
+  Treap tree;
+
+  ASSERT_EQ(tree.insert(5), true);
+  ASSERT_EQ(tree.insert(6), true);
+  ASSERT_EQ(tree.insert(5), false);
+  ASSERT_EQ(tree.find(5), true);
+  ASSERT_EQ(tree.find(7), false);
+
+  tree.erase(5);
+
+  ASSERT_EQ(tree.find(5), false);
+}
--- task_05/src/tree.hpp
+++ task_05/src/tree.hpp
@@ -2,13 +2,69 @@
 
 #include <cstdlib>
 #include <map>
-struct Node {
-  int value, prior;
-  Node* l_child = 0;
-  Node* r_child = 0;
-  Node(int _key) { value = _key, prior = rand(); }
+#include <random>
 
-  Node* merge(Node* l, Node* r);
+struct Treap {
+  static std::minstd_rand generate;
+  struct Node {
+    int key, priority;
 
-  std::pair<Node*, Node*> split(Node* p, int x);
+    Node *l = nullptr, *r = nullptr;
+    Node(int key) : key(key), priority(generate()) {}
+  } *root = nullptr;
+
+  Node *merge(Node *a, Node *b) {
+    if (!a or !b) return a ? a : b;
+    if (a->priority > b->priority) {
+      a->r = merge(a->r, b);
+      return a;
+    } else {
+      b->l = merge(a, b->l);
+      return b;
+    }
+  }
+
+  void split(Node *n, int key, Node *&a, Node *&b) {
+    if (!n) {
+      a = b = nullptr;
+      return;
+    }
+    if (n->key < key) {
+      split(n->r, key, n->r, b);
+      a = n;
+    } else {
+      split(n->l, key, a, n->l);
+      b = n;
+    }
+  }
+
+ public:
+  inline bool find(int key) {
+    Node *less, *equal, *greater;
+    split(root, key, less, greater);
+    split(greater, key + 1, equal, greater);
+    bool result = equal;
+    root = merge(merge(less, equal), greater);
+    return result;
+  }
+
+  inline bool insert(int key) {
+    if (!find(key)) {
+      Node *less, *greater;
+      split(root, key, less, greater);
+      root = merge(merge(less, new Node(key)), greater);
+      return true;
+    } else
+      return false;
+  }
+
+  inline void erase(int key) {
+    Node *less, *equal, *greater;
+    split(root, key, less, greater);
+    split(greater, key + 1, equal, greater);
+    equal = merge(equal->l, equal->r);
+    root = merge(less, greater);
+  }
 };
+
+inline std::minstd_rand Treap::generate;

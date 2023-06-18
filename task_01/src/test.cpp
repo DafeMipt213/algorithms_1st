fa02297a8863398b248dd5d32c82448d4bdabf45
kirill yartsev <115335212+yunhkirill@users.noreply.github.com>
2023-06-18 00:05:20 +0000

    clang format

--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -7,22 +7,22 @@ TEST(StackTest, ComplexInt) {
   Stack<int> stack;
   ASSERT_ANY_THROW(stack.Pop());
   ASSERT_EQ(stack.Size(), 0);
-  stack.Push(1);              // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  stack.Push(3);              // Stack [1, 3]
-  ASSERT_EQ(stack.Pop(), 3);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  stack.Push(3);              // Stack [1, 2, 3]
-  stack.Push(4);              // Stack [1, 2, 3, 4]
+  stack.Push(1);             // Stack [1]
+  ASSERT_EQ(stack.Pop(), 1); // Stack []
+  stack.Push(1);             // Stack [1]
+  stack.Push(2);             // Stack [1, 2]
+  ASSERT_EQ(stack.Pop(), 2); // Stack [1]
+  ASSERT_EQ(stack.Pop(), 1); // Stack []
+  stack.Push(1);             // Stack [1]
+  stack.Push(2);             // Stack [1, 2]
+  ASSERT_EQ(stack.Pop(), 2); // Stack [1]
+  stack.Push(3);             // Stack [1, 3]
+  ASSERT_EQ(stack.Pop(), 3); // Stack [1]
+  ASSERT_EQ(stack.Pop(), 1); // Stack []
+  stack.Push(1);             // Stack [1]
+  stack.Push(2);             // Stack [1, 2]
+  stack.Push(3);             // Stack [1, 2, 3]
+  stack.Push(4);             // Stack [1, 2, 3, 4]
   ASSERT_EQ(stack.Size(), 4);
   ASSERT_EQ(stack.Back(), 4);
   ASSERT_EQ(stack.Size(), 4);
@@ -34,22 +34,22 @@ TEST(StackTest, ComplexFloat) {
   Stack<double> stack;
   ASSERT_ANY_THROW(stack.Pop());
   ASSERT_EQ(stack.Size(), 0);
-  stack.Push(1.3);              // Stack [1.3]
-  ASSERT_EQ(stack.Pop(), 1.3);  // Stack []
-  stack.Push(1.5);              // Stack [1.5]
-  stack.Push(2.1);              // Stack [1.5, 2.1]
-  ASSERT_EQ(stack.Pop(), 2.1);  // Stack [1.5]
-  ASSERT_EQ(stack.Pop(), 1.5);  // Stack []
-  stack.Push(1.5);              // Stack [1.5]
-  stack.Push(2.1);              // Stack [1.5, 2.1]
-  ASSERT_EQ(stack.Pop(), 2.1);  // Stack [1.5]
-  stack.Push(3.3);              // Stack [1.5, 3.3]
-  ASSERT_EQ(stack.Pop(), 3.3);  // Stack [1.5]
-  ASSERT_EQ(stack.Pop(), 1.5);  // Stack []
-  stack.Push(4.);               // Stack [1.]
-  stack.Push(2.);               // Stack [1., 2.]
-  stack.Push(3.);               // Stack [1., 2., 3.]
-  stack.Push(4.);               // Stack [1., 2., 3., 4.]
+  stack.Push(1.3);             // Stack [1.3]
+  ASSERT_EQ(stack.Pop(), 1.3); // Stack []
+  stack.Push(1.5);             // Stack [1.5]
+  stack.Push(2.1);             // Stack [1.5, 2.1]
+  ASSERT_EQ(stack.Pop(), 2.1); // Stack [1.5]
+  ASSERT_EQ(stack.Pop(), 1.5); // Stack []
+  stack.Push(1.5);             // Stack [1.5]
+  stack.Push(2.1);             // Stack [1.5, 2.1]
+  ASSERT_EQ(stack.Pop(), 2.1); // Stack [1.5]
+  stack.Push(3.3);             // Stack [1.5, 3.3]
+  ASSERT_EQ(stack.Pop(), 3.3); // Stack [1.5]
+  ASSERT_EQ(stack.Pop(), 1.5); // Stack []
+  stack.Push(4.);              // Stack [1.]
+  stack.Push(2.);              // Stack [1., 2.]
+  stack.Push(3.);              // Stack [1., 2., 3.]
+  stack.Push(4.);              // Stack [1., 2., 3., 4.]
   ASSERT_EQ(stack.Size(), 4);
   ASSERT_EQ(stack.Back(), 4.);
   ASSERT_EQ(stack.Size(), 4);
@@ -60,73 +60,73 @@ TEST(StackTest, ComplexFloat) {
 TEST(StackTest, SimpleChar) {
   Stack<char> stack;
   ASSERT_ANY_THROW(stack.Pop());
-  stack.Push('h');  // Stack ['b']
-  stack.Push('l');  // Stack ['b', 'l']
-  stack.Push('a');  // Stack ['b', 'l', 'a']
+  stack.Push('h'); // Stack ['b']
+  stack.Push('l'); // Stack ['b', 'l']
+  stack.Push('a'); // Stack ['b', 'l', 'a']
   ASSERT_EQ(stack.Size(), 3);
-  ASSERT_EQ(stack.Pop(), 'a');  // Stack ['b', 'l']
-  stack.Clear();                // Stack [ ]
+  ASSERT_EQ(stack.Pop(), 'a'); // Stack ['b', 'l']
+  stack.Clear();               // Stack [ ]
   ASSERT_EQ(stack.Size(), 0);
 }
 
 TEST(MinStackTest, ComplexInt) {
   MinStack<int> stack;
   ASSERT_ANY_THROW(stack.Pop());
-  stack.Push(1);  // Stack [1]
+  stack.Push(1); // Stack [1]
   ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
+  ASSERT_EQ(stack.Pop(), 1); // Stack []
+  stack.Push(1);             // Stack [1]
+  stack.Push(2);             // Stack [1, 2]
   ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(2);              // Stack [2]
-  stack.Push(1);              // Stack [2, 1]
+  ASSERT_EQ(stack.Pop(), 2); // Stack [1]
+  ASSERT_EQ(stack.Pop(), 1); // Stack []
+  stack.Push(2);             // Stack [2]
+  stack.Push(1);             // Stack [2, 1]
   ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 1);  // Stack [2]
-  stack.Push(3);              // Stack [2, 3]
+  ASSERT_EQ(stack.Pop(), 1); // Stack [2]
+  stack.Push(3);             // Stack [2, 3]
   ASSERT_EQ(stack.GetMin(), 2);
-  ASSERT_EQ(stack.Pop(), 3);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 2);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  stack.Push(3);              // Stack [1, 2, 3]
-  stack.Push(4);              // Stack [1, 2, 3, 4]
+  ASSERT_EQ(stack.Pop(), 3); // Stack [1]
+  ASSERT_EQ(stack.Pop(), 2); // Stack []
+  stack.Push(1);             // Stack [1]
+  stack.Push(2);             // Stack [1, 2]
+  stack.Push(3);             // Stack [1, 2, 3]
+  stack.Push(4);             // Stack [1, 2, 3, 4]
   ASSERT_EQ(stack.Size(), 4);
   ASSERT_EQ(stack.Back(), 4);
   ASSERT_EQ(stack.Size(), 4);
   stack.Clear();
   ASSERT_EQ(stack.Size(), 0);
-  stack.Push(5);              // Stack [5]
-  stack.Push(4);              // Stack [5, 4]
-  stack.Push(4);              // Stack [5, 4, 4]
-  ASSERT_EQ(stack.Pop(), 4);  // Stack [5, 4]
+  stack.Push(5);             // Stack [5]
+  stack.Push(4);             // Stack [5, 4]
+  stack.Push(4);             // Stack [5, 4, 4]
+  ASSERT_EQ(stack.Pop(), 4); // Stack [5, 4]
   ASSERT_EQ(stack.GetMin(), 4);
 }
 
 TEST(MinStackTest, ComplexFloat) {
   MinStack<double> stack;
   ASSERT_ANY_THROW(stack.Pop());
-  stack.Push(1.2);  // Stack [1.2]
+  stack.Push(1.2); // Stack [1.2]
   ASSERT_EQ(stack.GetMin(), 1.2);
-  ASSERT_EQ(stack.Pop(), 1.2);  // Stack []
-  stack.Push(1.2);              // Stack [1.2]
-  stack.Push(2.2);              // Stack [1.2, 2.2]
+  ASSERT_EQ(stack.Pop(), 1.2); // Stack []
+  stack.Push(1.2);             // Stack [1.2]
+  stack.Push(2.2);             // Stack [1.2, 2.2]
   ASSERT_EQ(stack.GetMin(), 1.2);
-  ASSERT_EQ(stack.Pop(), 2.2);  // Stack [1.2]
-  ASSERT_EQ(stack.Pop(), 1.2);  // Stack []
-  stack.Push(1.3);              // Stack [1.3]
-  stack.Push(2.3);              // Stack [1.3, 2.3]
+  ASSERT_EQ(stack.Pop(), 2.2); // Stack [1.2]
+  ASSERT_EQ(stack.Pop(), 1.2); // Stack []
+  stack.Push(1.3);             // Stack [1.3]
+  stack.Push(2.3);             // Stack [1.3, 2.3]
   ASSERT_EQ(stack.GetMin(), 1.3);
-  ASSERT_EQ(stack.Pop(), 2.3);  // Stack [1.3]
-  stack.Push(3.3);              // Stack [1.3, 3.3]
+  ASSERT_EQ(stack.Pop(), 2.3); // Stack [1.3]
+  stack.Push(3.3);             // Stack [1.3, 3.3]
   ASSERT_EQ(stack.GetMin(), 1.3);
-  ASSERT_EQ(stack.Pop(), 3.3);  // Stack [1.3]
-  ASSERT_EQ(stack.Pop(), 1.3);  // Stack []
-  stack.Push(1.);               // Stack [1.]
-  stack.Push(2.);               // Stack [1., 2.]
-  stack.Push(3.);               // Stack [1., 2., 3.]
-  stack.Push(4.);               // Stack [1., 2., 3., 4.]
+  ASSERT_EQ(stack.Pop(), 3.3); // Stack [1.3]
+  ASSERT_EQ(stack.Pop(), 1.3); // Stack []
+  stack.Push(1.);              // Stack [1.]
+  stack.Push(2.);              // Stack [1., 2.]
+  stack.Push(3.);              // Stack [1., 2., 3.]
+  stack.Push(4.);              // Stack [1., 2., 3., 4.]
   ASSERT_EQ(stack.Size(), 4);
   ASSERT_EQ(stack.Back(), 4.);
   ASSERT_EQ(stack.Size(), 4);
@@ -137,13 +137,13 @@ TEST(MinStackTest, ComplexFloat) {
 TEST(MinStackTest, SimpleChar) {
   MinStack<char> stack;
   ASSERT_ANY_THROW(stack.Pop());
-  stack.Push('h');  // Stack ['b']
-  stack.Push('l');  // Stack ['b', 'l']
-  stack.Push('a');  // Stack ['b', 'l', 'a']
-  stack.Push('k');  // Stack ['b', 'l', 'a', 'k']
+  stack.Push('h'); // Stack ['b']
+  stack.Push('l'); // Stack ['b', 'l']
+  stack.Push('a'); // Stack ['b', 'l', 'a']
+  stack.Push('k'); // Stack ['b', 'l', 'a', 'k']
   ASSERT_EQ(stack.GetMin(), 'a');
   ASSERT_EQ(stack.Size(), 4);
-  ASSERT_EQ(stack.Pop(), 'k');  // Stack ['b', 'l', 'a']
-  stack.Clear();                // Stack [ ]
+  ASSERT_EQ(stack.Pop(), 'k'); // Stack ['b', 'l', 'a']
+  stack.Clear();               // Stack [ ]
   ASSERT_EQ(stack.Size(), 0);
 }
--- task_03/src/sort.hpp
+++ task_03/src/sort.hpp
@@ -2,10 +2,10 @@
 
 #include <vector>
 
-template <typename T>
-std::vector<T> QuickSort(const std::vector<T>& data) {
+template <typename T> std::vector<T> QuickSort(const std::vector<T> &data) {
   std::vector<T> result = data;
-  if (data.empty()) return result;
+  if (data.empty())
+    return result;
   std::vector<int> left_and_right = {0, int(data.size()) - 1};
   do {
     int right = left_and_right.back();
@@ -18,8 +18,10 @@ std::vector<T> QuickSort(const std::vector<T>& data) {
 
     T midElem = result[(j + i) / 2];
     do {
-      while (result[i] < midElem) ++i;
-      while (result[j] > midElem) --j;
+      while (result[i] < midElem)
+        ++i;
+      while (result[j] > midElem)
+        --j;
       if (i <= j) {
         std::swap(result[i], result[j]);
         ++i;

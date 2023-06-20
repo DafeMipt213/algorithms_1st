5dfcb9edc6bb5935c56c5f0057f23af989e36598
VladimirSpe <75093117+VladimirSpe@users.noreply.github.com>
2023-06-20 07:27:20 +0000

    task_1

---
+++ task_01/src/stack.cpp
@@ -0,0 +1,48 @@
+#include "stack.hpp"
+
+void Stack::Push(int value) {
+  NodeStack<int>* p;
+  p = new NodeStack<int>;
+  p->item = value;
+  p->next = Top;
+  Top = p;
+}
+
+int Stack::Pop() {
+  if (Top == nullptr) {
+    return 0;
+  }
+  NodeStack<int>* p;
+  int value = Top->item;
+  p = Top;
+  Top = Top->next;
+  delete p;
+  return value;
+}
+
+void MinStack::Push(int value) {
+  minNodeStack<int>* p;
+  p = new minNodeStack<int>;
+  if (Top == nullptr || Top->nowmin > value) {
+    p->nowmin = value;
+  } else {
+    p->nowmin = Top->nowmin;
+  }
+  p->item = value;
+  p->next = Top;
+  Top = p;
+}
+
+int MinStack::Pop() {
+  if (Top == nullptr) {
+    return 0;
+  }
+  minNodeStack<int>* p;
+  int value = Top->item;
+  p = Top;
+  Top = Top->next;
+  delete p;
+  return value;
+}
+
+int MinStack::GetMin() { return Top->nowmin; }

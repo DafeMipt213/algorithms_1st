30b3781b2f34c48b6b77e8758941ae5d82d09b45
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-06-21 06:07:10 +0000

    clang fixed

--- task_04/src/main.cpp
+++ task_04/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <order_statistics.hpp>
+
+#include "order_statistics.hpp"
 
 int main() {
   std::vector<int> p{1, 2, 5, 1, 7, 9, 11, 213, 12, 51, 17, 99};    // NOLINT

acf4bde39d14cae7ad3dcad3cacbd3f7d1b0594f
goldleprikon <118297571+goldleprikon@users.noreply.github.com>
2023-06-21 08:45:51 +0000

    fixing the first tasks

--- task_03/src/sort.cpp
+++ task_03/src/sort.cpp
@@ -2,9 +2,38 @@
 
 #include <algorithm>
 #include <vector>
-
+/*
+def bose_nelson_merge(j, r, m):
+    if j + r < len(data):
+        if m == 1:
+            if data[j] > data[j + r]:
+                data[j], data[j + r] = data[j + r], data[j]
+        else:
+            m = m // 2
+            bose_nelson_merge(j, r, m)
+            if j + r + m < len(data):
+                bose_nelson_merge(j + m, r, m)
+            bose_nelson_merge(j + m, r - m, m)
+    return data
+*/
+/*
+void Merge(int j, int r, int m) {
+  if (j + r < data.size()) {
+    if (m == 1) {
+      if ( data[j] > data[j + r]) {
+        int buffer = data[j];
+        data[j] =  data[j + r];
+        data[j + r] = data[j];
+      }
+    }
+  }
+}
+*/
+//what
 std::vector<int> Sort(const std::vector<int>& data) {
   std::vector<int> result = data;
+
   std::sort(result.begin(), result.end());
+  
   return result;
 }

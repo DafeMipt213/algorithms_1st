0fddd9a28d560d560b3c96827edf6d556495590e
goldleprikon <118297571+goldleprikon@users.noreply.github.com>
2023-06-21 17:17:44 +0000

    all without a sixth

--- task_03/src/sort.cpp
+++ task_03/src/sort.cpp
@@ -1,37 +1,31 @@
 #include "sort.hpp"
 
-#include <algorithm>
+void qsort(std::vector<int>::iterator a, size_t low, size_t hight) {
+  int i = low;
+  int j = hight;
+  int pivot = a[(i + j) / 2];
+  int temp{0};
 
-int* partition(int* begin, int* end) {
-  int pivot = *end;
- 
-  int* p_ind = begin;
- 
-  for (int* i = begin; i < end; i++) {
-      if (*i <= pivot) {
-        std::swap(*i, *p_ind);
-        p_ind++;
-      }
+  while (i <= j) {
+    while (a[i] < pivot) i++;
+    while (a[j] > pivot) j--;
+    if (i <= j) {
+      std::swap(a[i], a[j]);
+      i++;
+      j--;
+    }
   }
-  std::swap (*p_ind, *end);
- 
-  return p_ind;
-}
- 
-void qsort(int* begin, int* end) {
-  if (begin >= end) {
-      return;
-  }
-  int* pivot = partition(begin, end);
- 
-  qsort(begin, pivot - 1); 
-  qsort(pivot + 1, end);
+
+  if (j > low) 
+    qsort(a, low, j);
+  if (i < hight) 
+    qsort(a, i, hight);
 }
 
-std::vector<int> Sort(const std::vector<int>& data) {
-  std::vector<int> result = data;
-
-  qsort(&result[0], &result[result.size() - 1]);
-
-  return result;
+std::vector<int> Sort(std::vector<int> vec) {
+  if (vec.size() == 0) return {};
+  if (vec.size() == 1) return vec;
+  std::vector<int>::iterator i = vec.begin();
+  qsort(i, 0, (int)vec.size() - 1);
+  return vec;
 }

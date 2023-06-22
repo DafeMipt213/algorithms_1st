69dd11e98326a9a09502a42fdc70303587ea1d57
VladimirSpe <75093117+VladimirSpe@users.noreply.github.com>
2023-06-22 20:33:18 +0000

    Merge_Sort added

---
+++ task_03/src/Merge_Sort.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <vector>
+void merge(std::vector<int>& a, int start, int end, int mid);
+std::vector<int> Sort(const std::vector<int>& data);
+std::vector<int> merge_sort(std::vector<int>& a, int start, int end);
